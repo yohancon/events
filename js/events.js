7bc61ebe0435dd32588dcc68ab5015aa85bf4e26
needarrest <117890014+needarrest@users.noreply.github.com>
2024-09-10 22:44:11 +0800

    Create Just Announced filter
    - Create Just Announced filter for shows that are recently announced.
    - Minimize font size for venues
    - Add content on js files

--- js/events.js
+++ js/events.js
@@ -25,6 +25,13 @@ const events = [
     },
     {
         artist: [
+                    "SB19",
+                ],
+        date: new Date("2024-10-26T20:00:00"),
+        venue: venues.araneta
+    },
+    {
+        artist: [
                     "Zild",
                     "One Click Straight",
                     "Shanni",
@@ -124,6 +131,7 @@ const events = [
                     "Maroon 5",
                 ],
         date: new Date("2025-01-29"),
+        announced: new Date("2024-09-09T11:41:00"),
         venue: venues.moaarena
     },
     {
@@ -458,11 +466,427 @@ const events = [
         venue: venues.musicmuseum
     },
     {
+        name: "Jetcoaster Karaoke Single Launch",
+        artist: [
+                    "Jetcoaster",
+                    "Sunflower Station",
+                    "Shoulder State",
+                    "The Revisors",
+                ],
+        date: new Date("2024-09-13T20:00:00"),
+        venue: venues.rabbithole
+    },
+    {
+        artist: [
+                    "Agaw Agimat",
+                    "Anibughaw",
+                    "Signum",
+                    "Eskalera",
+                    "Dally",
+                ],
+        date: new Date("2024-09-28T21:00:00"),
+        venue: venues.bistro
+    },
+    {
+        artist: [
+                    "Adjeng",
+                    "Barbie Almalbis",
+                ],
+        date: new Date("2024-09-17"),
+        venue: venues.redrhino
+    },
+    {
+        name: "SetYEAHmbre!: Isang Improv Show by SPIT",
+        artist: [
+                    "SPIT",
+                ],
+        date: new Date("2024-09-10T21:00:00"),
+        venue: venues.twelvemonkeys
+    },
+    {
+        artist: [
+                    "Nina",
+                    "Peter Caimbon",
+                ],
+        date: new Date("2024-09-11T21:30:00"),
+        venue: venues.twelvemonkeys
+    },
+    {
+        name: "Red Letter Day: Maverick Productions' 2nd Anniversary Day 1",
+        artist: [
+                    "One Click Straight",
+                    "Oh, Flamingo!",
+                    "Yun",
+                    "Jurassic Pards",
+                    "F'in Quarrel",
+                    "Charleston Music",
+                    "Jyve",
+                    "That Band Astra",
+                    "Band Ni Cat",
+                ],
+        date: new Date("2024-11-15"),
+        venue: venues.milenya
+    },
+    {
+        name: "Red Letter Day: Maverick Productions' 2nd Anniversary Day 2",
+        artist: [
+                    "Snakefight",
+                    "Simpatiko PH",
+                    "Plus Two",
+                    "Blind Side",
+                    "Chains4no1",
+                    "Comic's Trip",
+                    "Sidra",
+                    "Achilles Vee",
+                ],
+        date: new Date("2024-11-16"),
+        venue: venues.bossbar
+    },
+    {
+        artist: [
+                    "Armi Millare",
+                    "The Plug",
+                ],
+        date: new Date("2024-09-11T21:30:00"),
+        venue: venues.hardrockmnl
+    },
+    {
+        name: "Maskipaps: The Crossover",
+        artist: [
+                    "Maki",
+                    "Demi",
+                    "Over October",
+                ],
+        date: new Date("2024-12-20"),
+        venue: venues.upsunken
+    },
+    {
+        name: "Melodies & Memories",
+        artist: [
+                    "The Company",
+                    "Dylan Menor",
+                ],
+        date: new Date("2024-09-14"),
+        venue: venues.rmgalleria
+    },
+    {
+        name: "The Best of David Archuleta",
+        artist: [
+                    "David Archuleta",
+                ],
+        date: new Date("2024-09-14"),
+        venue: venues.frontier
+    },
+    {
+        artist: [
+                    "Arthur Nery",
+                ],
+        date: new Date("2024-10-25T20:00:00"),
+        venue: venues.araneta
+    },
+    {
+        name: "45th Manila International Book Fair",
+        artist: [
+                    "Cloud 7",
+                ],
+        date: new Date("2024-09-14T13:45:00"),
+        venue: venues.smxmanila
+    },
+    {
+        name: "45th Manila International Book Fair",
+        artist: [
+                    "ASTER",
+                    "AJAA",
+                ],
+        date: new Date("2024-09-15T13:45:00"),
+        venue: venues.smxmanila
+    },
+    {
+        name: "Soundtrip Sessions Vol. 3",
+        artist: [
+                    "Southborder",
+                    "Ella May Saison",
+                ],
+        date: new Date("2024-11-09T20:00:00"),
+        venue: venues.theatresolaire
+    },
+    {
+        name: "SIGA Two Leg Tour",
+        artist: [
+                    "One Click Straight",
+                    "Chain Messages",
+                    "Matoki",
+                    "Silent Discc",
+                    "Medyo Maybe",
+                ],
+        date: new Date("2024-09-14T19:30:00"),
+        venue: venues.hangoutresto
+    },
+    {
+        name: "Wish 107.5",
+        artist: [
+                    "Eliza Maturan",
+                    "Yah Bles",
+                ],
+        date: new Date("2024-09-10T16:00:00"),
+        venue: venues.etoncentris
+    },
+    {
+        name: "Bonded by Sound",
+        artist: [
+                    "Side A",
+                    "Janine Teñoso",
+                ],
+        date: new Date("2024-09-21T20:00:00"),
+        venue: venues.theatresolaire
+    },
+    {
+        name: "Fiji Blue Asia Glide Tour",
+        artist: [
+                    "Fiji Blue",
+                ],
+        date: new Date("2024-11-29"),
+        announced: new Date("2024-09-10T12:00:00"),
+        venue: venues.samsunghall
+    },
+    {
+        name: "Olivia Rodrigo Manila Silver Star Show",
+        artist: [
+                    "Olivia Rodrigo",
+                ],
+        date: new Date("2024-11-05"),
+        announced: new Date("2024-09-10T12:06:00"),
+        venue: venues.pharena
+    },
+    {
+        artist: [
+                    "Nina",
+                    "Sound Ministry",
+                ],
+        date: new Date("2024-09-14"),
+        venue: venues.districtone
+    },
+    {
+        name: "Gary V Back @ MC",
+        artist: [
+                    "Gary Valenciano",
+                    "Rhythm N Babes",
+                    "Sayawatha",
+                ],
+        date: new Date("2024-10-11T18:30:00"),
+        venue: venues.marianauditorium
+    },
+    {
+        name: "The Bootleg Beatles Manila 2024",
+        artist: [
+                    "The Bootleg Beatles",
+                ],
+        date: new Date("2024-10-26T20:00:00"),
+        venue: venues.theatresolaire
+    },
+    {
+        artist: [
+                    "gilcharmm",
+                    "Kino1998",
+                    "The Nomads",
+                    "Jerry Something",
+                    "Vacant Signs",
+                    "Six To Cast",
+                    "Galaxy Lodge",
+                    "Parasouls",
+                    "Afuera",
+                    "Achilles Vee",
+                ],
+        date: new Date("2024-09-12"),
+        venue: venues.eightyeight
+    },
+    {
+        name: "Asia Tour 2024",
+        artist: [
+                    "Big Time Rush",
+                ],
+        date: new Date("2024-10-17"),
+        venue: venues.frontier
+    },
+    {
+        name: "Queers Night Vol. 2: Young, Pristine, in a Fever Dream",
+        artist: [
+                    "Walktrip",
+                    "Doghause",
+                    "Lu",
+                    "Joan Velasquez",
+                    "PK Shellboy",
+                    "Wishy Washy",
+                ],
+        date: new Date("2024-09-29"),
+        venue: venues.eightyeight
+    },
+    {
+        name: "Press Hit Play Lampara Mall Tour",
+        artist: [
+                    "Press Hit Play",
+                ],
+        date: new Date("2024-09-14T16:00:00"),
+        venue: venues.malaboncitisquare
+    },
+    {
+        name: "WHEN WE WERE YOUNG Cover Night Vol. 2",
+        artist: [
+                    "Anthem for Maria",
+                    "Hello Mellow",
+                    "DON'T WAVE",
+                    "Ilayà",
+                    "Do Not Delay",
+                    "Armed Like Giants",
+                    "Sidra",
+                    "Colovrwave",
+                    "Breakfast & Breakdowns",
+                    "Rue Monday",
+                ],
+        date: new Date("2024-09-28T18:00:00"),
+        venue: venues.cityboymain
+    },
+    {
+        name: "Chapter 15: In A Hole In The Ground",
+        artist: [
+                    "Pinkmen",
+                    "Lockout Season",
+                    "Lemlunay",
+                    "Trailing Noises",
+                    "ACT!",
+                    "Breē",
+                    "Tema",
+                    "Forest Rituals",
+                ],
+        date: new Date("2024-09-14T20:00:00"),
+        venue: venues.unknownpub
+    },
+    {
+        name: "PPOPCON PPOP Stage MIBF",
+        artist: [
+                    "1ST ONE",
+                    "KAIA",
+                    "New:ID",
+                    "Chin",
+                ],
+        date: new Date("2024-09-14T14:30:00"),
+        announced: new Date("2024-09-09T21:00:00"),
+        venue: venues.smxmanila
+    },
+    {
+        artist: [
+                    "Join the Club",
+                    "Jana Garcia",
+                    "Alon",
+                ],
+        date: new Date("2024-09-20"),
+        venue: venues.takeover
+    },
+    {
+        name: "G Fest 2024",
+        artist: [
+                    "BINI",
+                    "Denise Julia",
+                    "Jason Dhakal",
+                    "SunKissed Lola",
+                    "ASTER",
+                ],
+        date: new Date("2024-09-22"),
+        venue: venues.smxmanila
+    },
+    {
+        name: "Air Supply The Lost in Love Experience",
+        artist: [
+                    "Air Supply",
+                ],
+        date: new Date("2024-12-05"),
+        venue: venues.smxbacolod
+    },
+    {
+        name: "SiKAT v.3",
+        artist: [
+                    "Katrina Velarde",
+                ],
+        date: new Date("2024-10-11T20:00:00"),
+        venue: venues.frontier
+    },
+    {
+        name: "ASIYA Asian Music Festival & Conference",
+        artist: [
+                    "Hyukoh",
+                    "Sunset Rollercoaster",
+                    "Babychair",
+                    "Colde",
+                    "Flu",
+                    "Gabba",
+                    "Imase",
+                    "Numcha",
+                    "Reese Lansangan",
+                    "UDD",
+                    "Urbandub",
+                ],
+        date: new Date("2024-10-12"),
+        venue: venues.worldtrade
+    },
+    {
+        name: "ASIYA Asian Music Festival & Conference",
+        artist: [
+                    "BINI",
+                    "Balming Tiger",
+                    "Jose Miguel",
+                    "Juan Karlos",
+                    "Salasa",
+                    "Shye",
+                    "Ysanygo",
+                    "Zild",
+                    "9M88",
+                ],
+        date: new Date("2024-10-13"),
+        venue: venues.worldtrade
+    },
+    {
+        name: "Requiem Tour",
+        artist: [
+                    "keshi",
+                ],
+        date: new Date("2025-03-04"),
+        venue: venues.moaarena
+    },
+    {
+        name: "K-Swag Legend",
+        artist: [
+                    "Psy",
+                    "BBGirls",
+                    "The Art Connection",
+                    "PLUUS",
+                    "YGIG",
+                ],
+        date: new Date("2024-09-14T20:00:00"),
+        venue: venues.theatresolaire
+    },
+    {
+        name: "The Best of David Archuleta Live Press Conference and Pre-Concert Sneak Peek",
+        artist: [
+                    "David Archuleta",
+                ],
+        date: new Date("2024-09-13T18:00:00"),
+        announced: new Date("2024-09-10T13:17:00"),
+        venue: venues.marketmarket
+    },
+    {
+        artist: [
+                    "Bruno Major",
+                ],
+        date: new Date("2024-09-11T20:00:00"),
+        venue: venues.piccplenary
+    },
+    {
         name: "",
         artist: [
                     "",
                 ],
-        date: new Date("YYYY-MM-DDThh:mm:00"),
+        date: new Date("YYYY-MM-DDThh:mm:ss"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
         venue: venues.tba
     },
 ];
