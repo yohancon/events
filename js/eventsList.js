cea0c8b5cb1fc0896ef34e58ade07f4b0a575023
needarrest <hello.yohancon@gmail.com>
2024-09-25 16:03:32 +0800

    Update style, content list
    - Add background color to newly-announced entries
    - Adjust text-decoration to current page in nav and footer
    - Update event entries list

--- js/eventsList.js
+++ js/eventsList.js
@@ -14,6 +14,366 @@ const eventList = [
         announced: new Date("YYYY-MM-DDThh:mm:ss"),
     },
     {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Raymond Lauchengco",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.theatresolaire,
+        date: new Date("2024-11-23T20:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.mall,
+        paid: true,
+        title: "Hello, Love, Again Mall Tour",
+        mainAct: [
+            "Alden Richards",
+            "Kathryn Bernardo",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.amsolenad,
+        date: new Date("2024-10-05T00:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.mall,
+        paid: false,
+        title: "",
+        mainAct: [
+            "Manila Symphony Orchestra",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.amcircuit,
+        date: new Date("2024-09-29T18:00:00"),
+        announced: new Date("2024-09-16T21:50:00"),
+    },
+    {
+        type: eventType.mall,
+        paid: false,
+        title: "Bagong Pilipinas Pagkakaisa Concert",
+        mainAct: [
+            "Sandwich",
+            "Imago",
+            "Banda Ni Kleggy",
+            "Al James",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.vmnomo,
+        date: new Date("2024-09-27T17:00:00"),
+        announced: new Date("2024-09-16T17:00:00"),
+    },
+    {
+        type: eventType.gig,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Urbandub",
+            "Faintlight",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.bistro,
+        date: new Date("2024-10-04T21:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.mall,
+        paid: false,
+        title: "PPOP Fest",
+        mainAct: [
+            "Bituin",
+            "DIV9",
+            "GEM&I",
+            "IR1ZZ",
+            "GenZix",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.smmakati,
+        date: new Date("2024-09-27T17:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: false,
+        title: "Kyusiklaban Music Festival 2024",
+        mainAct: [
+            "",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.qmc,
+        date: new Date("2024-10-12T13:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "Davao Aurora Music Festival",
+        mainAct: [
+            "",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.crocodilepark,
+        date: new Date("2024-11-30T15:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Cigarettes After Sex",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.moaarena,
+        date: new Date("2025-01-14T20:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "Billboard Philippines Mainstage",
+        mainAct: [
+            "",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.moaarena,
+        date: new Date("2024-10-15T19:00:00"),
+        announced: new Date("2024-09-07T10:00:00"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Maroon 5",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.moaarena,
+        date: new Date("2025-01-29T00:00:00"),
+        announced: new Date("2024-09-09T11:41:00"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "NIKI",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.none,
+        date: new Date("2025-02-11T00:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "NIKI",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.none,
+        date: new Date("2025-02-12T00:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "The Script",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.araneta,
+        date: new Date("2025-02-11T20:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "The Script",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.araneta,
+        date: new Date("2025-02-12T20:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.fanmeet,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Jang Ki Yong",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.samsunghall,
+        date: new Date("2024-09-28T19:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.fanmeet,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Sailub Pon",
+            "Benz Garfield",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.teatrino,
+        date: new Date("2024-11-23T14:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.fanmeet,
+        paid: true,
+        title: "Last Twilight",
+        mainAct: [
+            "",
+        ],
+        guests: [
+            "Willian Lykn",
+        ],
+        venue: venues.uptheatre,
+        date: new Date("2024-10-12T15:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "Fusion x Cebu 2024",
+        mainAct: [
+            "",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.citydimare,
+        date: new Date("2024-10-05T16:00:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "A1",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.frontier,
+        date: new Date("2025-02-15T20:00:00"),
+        announced: new Date("2024-09-16T12:00:00"),
+    },
+    {
+        type: eventType.gig,
+        paid: true,
+        title: "",
+        mainAct: [
+            "Gino Padilla",
+        ],
+        guests: [
+            "Euphoric",
+        ],
+        venue: venues.bar360,
+        date: new Date("2024-09-28T22:45:00"),
+        announced: new Date("YYYY-MM-DDThh:mm:ss"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "SEVENTEEN",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.none,
+        date: new Date("2025-01-18T00:00:00"),
+        announced: new Date("2024-09-25T12:00:00"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "SEVENTEEN",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.none,
+        date: new Date("2025-01-19T00:00:00"),
+        announced: new Date("2024-09-25T12:00:00"),
+    },
+    {
+        type: eventType.concert,
+        paid: true,
+        title: "",
+        mainAct: [
+            "St. Vincent",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.filinvesttent,
+        date: new Date("2025-01-08T20:00:00"),
+        announced: new Date("2024-09-25T12:00:00"),
+    },
+    {
+        type: eventType.gig,
+        paid: false,
+        title: "Wish Bus",
+        mainAct: [
+            "Letters From June",
+        ],
+        guests: [
+            "",
+        ],
+        venue: venues.smpampanga,
+        date: new Date("2024-09-25T16:00:00"),
+        announced: new Date("2024-09-25T10:00:00"),
+    },
+    {
         type: eventType.gig,
         paid: true,
         title: "",
@@ -476,7 +836,11 @@ const eventList = [
         title: "Maskipaps: The Crossover",
         mainAct: [
             "The Ridleys",
-            "",
+            "Maki",
+            "Demi",
+            "Over October",
+            "Orange & Lemons",
+            "Cup of Joe",
         ],
         guests: [
             "",
@@ -554,7 +918,7 @@ const eventList = [
         ],
         venue: venues.samsunghall,
         date: new Date("2025-02-01T00:00:00"),
-        announced: new Date("2024-09-23Thh:mm:ss"),
+        announced: new Date("2024-09-23T00:00:00"),
     },
     {
         type: eventType.concert,
